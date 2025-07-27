490cee63b481f0568f40cf00079e89efe6c7b013
Nguyễn Khắc Huấn <smyth@Smyths-Mac-Mini.local>
2025-07-27 23:45:11 +0700

    Course

---
+++ app/Livewire/Back/Management/ActionsCourse.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Back\Management;
+
+use Livewire\Component;
+
+class ActionsCourse extends Component
+{
+    public function render()
+    {
+        return view('livewire.back.management.actions-course');
+    }
+}
---
+++ app/Livewire/Back/Management/Courses.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Back\Management;
+
+use Livewire\Component;
+
+class Courses extends Component
+{
+    public function render()
+    {
+        return view('livewire.back.management.courses');
+    }
+}
